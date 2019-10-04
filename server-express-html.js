792f4831fcfd265a8b737094b42ff32b7a4dc885
Johan <estiven141994@gmail.com>
2019-10-03 21:11:32 -0500

    confg puerto

--- server-express-html.js
+++ server-express-html.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
+const port = process.env.port || 3000
 
 app.use(express.static(__dirname + "/public_html"))
+app.listen(port);
 
-app.listen(3000) 
-
-console.log("Server en el puerto 3000");
+console.log(`Server en el puerto ${port}`);
